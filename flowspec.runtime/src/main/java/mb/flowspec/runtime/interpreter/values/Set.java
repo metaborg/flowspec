2e22df5bcf6f9318ee200c11be997cd375d9a441
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2018-03-20 14:19:43 +0100

    Remove all locking code.

--- flowspec.runtime/src/main/java/mb/flowspec/runtime/interpreter/values/Set.java
+++ flowspec.runtime/src/main/java/mb/flowspec/runtime/interpreter/values/Set.java
@@ -36,11 +36,6 @@ public class Set<K extends ITerm> implements IApplTerm {
     }
 
     @Override
-    public boolean isLocked() {
-        return false;
-    }
-
-    @Override
     public Multiset<ITermVar> getVars() {
         return ImmutableMultiset.of();
     }
@@ -90,8 +85,4 @@ public class Set<K extends ITerm> implements IApplTerm {
         return new ImmutableList.Builder<ITerm>().add(B.newList(this.set)).build();
     }
 
-    @Override
-    public IApplTerm withLocked(boolean locked) {
-        return this;
-    }
 }
