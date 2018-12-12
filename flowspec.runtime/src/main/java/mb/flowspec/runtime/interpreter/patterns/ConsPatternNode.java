6bbf15c074903c0d94f7bb15be37d509b577e339
Jeff Smits <jeff.smits@gmail.com>
2018-12-12 13:44:22 +0100

    Add linear-list rules as library, add Cons and Nil matching support

---
+++ flowspec.runtime/src/main/java/mb/flowspec/runtime/interpreter/patterns/ConsPatternNode.java
@@ -0,0 +1,54 @@
+package mb.flowspec.runtime.interpreter.patterns;
+
+import static mb.nabl2.terms.matching.TermMatch.M;
+
+import java.util.Optional;
+
+import org.metaborg.util.optionals.Optionals;
+
+import com.oracle.truffle.api.frame.FrameDescriptor;
+import com.oracle.truffle.api.frame.VirtualFrame;
+
+import mb.flowspec.runtime.interpreter.InitValues;
+import mb.nabl2.terms.IListTerm;
+import mb.nabl2.terms.matching.TermMatch.IMatcher;
+
+public class ConsPatternNode extends PatternNode {
+    private final PatternNode headPattern;
+    private final PatternNode tailPattern;
+
+    public ConsPatternNode(PatternNode headPattern, PatternNode tailPattern) {
+        this.headPattern = headPattern;
+        this.tailPattern = tailPattern;
+    }
+
+    @Override public boolean matchGeneric(VirtualFrame frame, Object value) {
+        if(!(value instanceof IListTerm)) {
+            return false;
+        }
+        return matchList(frame, (IListTerm) value);
+    }
+
+    private boolean matchList(VirtualFrame frame, IListTerm value) {
+        return M.cons(
+            (h, u) -> Optionals.when(headPattern.matchGeneric(frame, h)), 
+            (t, u) -> Optionals.when(tailPattern.matchGeneric(frame, t)), 
+            (c, h, t) -> c
+        ).match(value).isPresent();
+    }
+
+    @Override public void init(InitValues initValues) {
+        headPattern.init(initValues);
+        tailPattern.init(initValues);
+    }
+
+    public static IMatcher<ConsPatternNode> match(FrameDescriptor frameDescriptor) {
+        return M.appl2("Term", 
+                M.string(s -> Optionals.when(s.getValue().equals("Cons"))),
+                M.listElems(PatternNode.matchPattern(frameDescriptor))
+                    .flatMap(l -> l.size() == 2 ? Optional.of(l) : Optional.empty()),
+            (appl, consname, childPatterns) -> {
+                return new ConsPatternNode(childPatterns.get(0), childPatterns.get(1));
+            });
+    }
+}
---
+++ flowspec.runtime/src/main/java/mb/flowspec/runtime/interpreter/patterns/NilPatternNode.java
@@ -0,0 +1,43 @@
+package mb.flowspec.runtime.interpreter.patterns;
+
+import static mb.nabl2.terms.matching.TermMatch.M;
+
+import org.metaborg.util.optionals.Optionals;
+
+import com.oracle.truffle.api.frame.FrameDescriptor;
+import com.oracle.truffle.api.frame.VirtualFrame;
+
+import mb.flowspec.runtime.interpreter.InitValues;
+import mb.nabl2.terms.IListTerm;
+import mb.nabl2.terms.matching.TermMatch.IMatcher;
+
+public class NilPatternNode extends PatternNode {
+    public static final NilPatternNode SINGLETON = new NilPatternNode();
+
+    protected NilPatternNode() {
+    }
+
+    @Override public boolean matchGeneric(VirtualFrame frame, Object value) {
+        if(!(value instanceof IListTerm)) {
+            return false;
+        }
+        return matchList(frame, (IListTerm) value);
+    }
+
+    private boolean matchList(VirtualFrame frame, IListTerm value) {
+        return M.nil(n -> n).match(value).isPresent();
+    }
+
+    @Override public void init(InitValues initValues) {
+    }
+
+    public static IMatcher<NilPatternNode> match(FrameDescriptor frameDescriptor) {
+        return M.appl2("Term", 
+                M.string(s -> Optionals.when(s.getValue().equals("Nil"))),
+                M.listElems(PatternNode.matchPattern(frameDescriptor))
+                    .flatMap(l -> Optionals.when(l.isEmpty())),
+            (appl, consname, childPatterns) -> {
+                return NilPatternNode.SINGLETON;
+            });
+    }
+}
