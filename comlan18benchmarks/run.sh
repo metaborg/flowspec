be8e82a66b4c7e3a8ceb1de6a210eaf343267bd3
Jeff Smits <jeff.smits@gmail.com>
2018-12-16 22:06:36 +0100

    Fix run script to ignore larger benchmarks, fix spoofax-language built with broken code apparently

--- comlan18benchmarks/run.sh
+++ comlan18benchmarks/run.sh
@@ -4,10 +4,10 @@ mvn clean install
 mkdir -p results/flowspec
 mkdir -p results/stratego
 
-java -jar target/comlan18benchmarks.jar -f 1 -rff results/flowspec/libspoofax.csv ".*flowspec\.LibSpoofaxBenchmark"
-java -jar target/comlan18benchmarks.jar -f 1 -rff results/flowspec/libstratego.csv ".*flowspec\.LibStrategoBenchmark"
-java -jar target/comlan18benchmarks.jar -f 1 -rff results/flowspec/libstrc.csv ".*flowspec\.LibStrcBenchmark"
+java -jar target/comlan18benchmarks.jar -f 1 -rff results/flowspec/libspoofax.csv "FSLibSpoofaxBenchmark"
+# java -jar target/comlan18benchmarks.jar -f 1 -rff results/flowspec/libstratego.csv "FSLibStrategoBenchmark"
+# java -jar target/comlan18benchmarks.jar -f 1 -rff results/flowspec/libstrc.csv "FSLibStrcBenchmark"
 
-java -jar target/comlan18benchmarks.jar -f 1 -rff results/stratego/libspoofax.csv ".*stratego\.LibSpoofaxBenchmark"
-java -jar target/comlan18benchmarks.jar -f 1 -rff results/stratego/libstratego.csv ".*stratego\.LibStrategoBenchmark"
-java -jar target/comlan18benchmarks.jar -f 1 -rff results/stratego/libstrc.csv ".*stratego\.LibStrcBenchmark"
+java -jar target/comlan18benchmarks.jar -f 1 -rff results/stratego/libspoofax.csv "StrLibSpoofaxBenchmark"
+# java -jar target/comlan18benchmarks.jar -f 1 -rff results/stratego/libstratego.csv "StrLibStrategoBenchmark"
+# java -jar target/comlan18benchmarks.jar -f 1 -rff results/stratego/libstrc.csv "StrLibStrcBenchmark"
